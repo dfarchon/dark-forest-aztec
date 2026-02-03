8f08a50812df5431782a655f3a2f43417df9fa90
cherryblue1024 <cherryblue1024@gmail.com>
2026-02-04 05:46:52 +0800

    refactor(contracts): types & config

--- contracts/eslint.config.ts
+++ contracts/eslint.config.ts
@@ -6,27 +6,27 @@ import simpleImportSort from 'eslint-plugin-simple-import-sort';
 import prettierConfig from 'eslint-config-prettier';
 
 export default defineConfig([
-  globalIgnores(['dist', 'target']),
-  {
-    files: ['scripts/**/*.{ts,tsx,js,jsx}'],
-    extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      prettierConfig, // Disable ESLint rules that conflict with Prettier
-    ],
-    languageOptions: {
-      ecmaVersion: 2020,
-      parserOptions: {
-        sourceType: 'module',
-      },
-      globals: globals.node,
+    globalIgnores(['dist', 'target']),
+    {
+        files: ['scripts/**/*.{ts,tsx,js,jsx}'],
+        extends: [
+            js.configs.recommended,
+            tseslint.configs.recommended,
+            prettierConfig, // Disable ESLint rules that conflict with Prettier
+        ],
+        languageOptions: {
+            ecmaVersion: 2020,
+            parserOptions: {
+                sourceType: 'module',
+            },
+            globals: globals.node,
+        },
+        plugins: {
+            'simple-import-sort': simpleImportSort,
+        },
+        rules: {
+            'simple-import-sort/imports': 'error',
+            'simple-import-sort/exports': 'error',
+        },
     },
-    plugins: {
-      'simple-import-sort': simpleImportSort,
-    },
-    rules: {
-      'simple-import-sort/imports': 'error',
-      'simple-import-sort/exports': 'error',
-    },
-  },
 ]);
